9893c0b782850d081032eef7b0346ee3b1249872
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2024-09-27 20:05:27 +0900

    Update map.dart

---
+++ lib/map.dart
@@ -0,0 +1,178 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+
+class Map extends StatelessWidget {
+  const Map({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Naver Map UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const NaverMapScreen(),
+    );
+  }
+}
+
+class NaverMapScreen extends StatefulWidget {
+  const NaverMapScreen({Key? key}) : super(key: key);
+
+  @override
+  _NaverMapScreenState createState() => _NaverMapScreenState();
+}
+
+class _NaverMapScreenState extends State<NaverMapScreen> {
+  final Completer<NaverMapController> mapControllerCompleter = Completer();
+
+  Set<String> _selectedFilters = {'ALL'};
+
+  void _toggleFilter(String filter) {
+    setState(() {
+      if (filter == 'ALL') {
+        // ALL 선택 시 다른 필터를 해제
+        _selectedFilters.clear();
+        _selectedFilters.add('ALL');
+      } else {
+        // 다른 필터 선택 시 ALL 해제
+        if (_selectedFilters.contains('ALL')) {
+          _selectedFilters.remove('ALL');
+        }
+        if (_selectedFilters.contains(filter)) {
+          _selectedFilters.remove(filter);
+        } else {
+          _selectedFilters.add(filter);
+        }
+      }
+      // 선택된 필터가 없을 시 자동으로 ALL 선택
+      if (_selectedFilters.isEmpty) {
+        _selectedFilters.add('ALL');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: [
+          NaverMap(
+            options: const NaverMapViewOptions(
+              indoorEnable: true,
+              locationButtonEnable: true,
+              consumeSymbolTapEvents: false,
+            ),
+            onMapReady: (controller) async {
+              mapControllerCompleter.complete(controller);
+              log("onMapReady", name: "onMapReady");
+            },
+          ),
+          Column(
+            children: [
+              // 검색창
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 10, 16, 5),
+                child: Container(
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(1.0),
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4.0,
+                        offset: Offset(0, 0),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: '검색어를 입력해주세요',
+                      prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.symmetric(vertical: 14),
+                    ),
+                  ),
+                ),
+              ),
+              // 필터
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  // padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  padding: EdgeInsets.fromLTRB(16, 5, 16, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ...['ALL', '범죄', '건강위해', '안전사고', '자연재해', '재난', '동식물', '기타'].map(
+                            (filter) => MapFilterChip(
+                          label: filter,
+                          isSelected: _selectedFilters.contains(filter),
+                          onSelected: () => _toggleFilter(filter),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MapFilterChip extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+  final VoidCallback onSelected;
+
+  const MapFilterChip({
+    Key? key,
+    required this.label,
+    required this.isSelected,
+    required this.onSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: GestureDetector(
+        onTap: onSelected,
+        child: Container(
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.redAccent : Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                offset: Offset(0, 0)
+              ),
+            ],
+            border: Border.all(
+              color: isSelected ? Colors.redAccent : Colors.grey.shade300,
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+            child: Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? Colors.white : Colors.black,
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
